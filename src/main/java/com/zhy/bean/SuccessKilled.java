483bcb1f754f307280112987689de047d040d590
zhy <zhyzyn@126.com>
2016-05-26 13:56:21 +0800

    2016-4-29 SpringMVC+Mybatis 整合成功

---
+++ src/main/java/com/zhy/bean/SuccessKilled.java
@@ -0,0 +1,63 @@
+package com.zhy.bean;
+
+import java.util.Date;
+
+public class SuccessKilled {
+    private long seckillId;
+    private long userPhone;
+    private int state;
+    private Date createDate;
+
+    private Seckill seckill;
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public long getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(long userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Seckill getSeckill() {
+        return seckill;
+    }
+
+    public void setSeckill(Seckill seckill) {
+        this.seckill = seckill;
+    }
+
+    @Override
+    public String toString() {
+        return "SuccessKilled{" +
+                "seckillId=" + seckillId +
+                ", userPhone=" + userPhone +
+                ", state=" + state +
+                ", createDate=" + createDate +
+                ", seckill=" + seckill +
+                '}';
+    }
+}
